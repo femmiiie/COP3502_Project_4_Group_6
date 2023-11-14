d51d4f7ba63415dac07473a76a2ef4b41ef1b0a9
Sandro Mocevic <sandro.mocev@gmail.com>
2023-11-13 21:23:32 -0500

    added box list getter

--- board.py
+++ board.py
@@ -11,13 +11,23 @@ class Board:
 
     '''GETTERS FOR VALIDATION METHODS'''
     #Gets values from specified row as ints
-    def get_row(self, num:int):
-        lst = []
+    def get_row(self, num:int)->list[int]:
+        value_list = []
         for i in self.board[num]:
             if i.get_cell_value() == 0:
-                lst.append(i.get_sketched_value())
+                value_list.append(i.get_sketched_value())
             else:
-                lst.append(i.get_cell_value())
-        return lst
+                value_list.append(i.get_cell_value())
+        return  value_list
 
-    
+    #Gets value from specified box as int list in reading order
+    def get_box(self, row_start:int, col_start:int)->list[int]:
+        value_list = []
+        for i in range(row_start, row_start+3):
+            for j in range(col_start, col_start+3):
+                if self.board[i][j].get_cell_value() == 0:
+                    value_list.append(self.board[i][j].get_sketched_value())
+                else:
+                    value_list.append(self.board[i][j].get_cell_value())
+        return  value_list
+        
