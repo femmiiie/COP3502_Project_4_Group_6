46a540ca3e2f054e064e87d106438115638167ff
Sandro Mocevic <sandro.mocev@gmail.com>
2023-11-18 20:48:24 -0500

    updated test file

--- sudoku_tester.py
+++ sudoku_tester.py
@@ -3,22 +3,34 @@ from board import Board
 
 def test_SudokuGenerator():
     instance = SudokuGenerator(9, 30)
-    #Should Print 2D 9x9 list of 0's
-    instance.print_board()
 
+        #PRINT FUNCS ARE WORKING
+        #BY EXTENSION GET BOARD WORKS
+    instance.print_board()
+    print()
+
+        #VALIDITY FUNCS ARE WORKING
+    #print(instance.valid_in_row(0,6))
+    #print(instance.is_valid(0,0, 6))
     instance.fill_values()
 
     #Should Print valid Sudoku Solution
     instance.print_board()
+    print()
+
+    #Should appropriately remove 30 cells
+    instance.remove_cells()
+    instance.print_board()
+
 
 
 def test_Board():
     instance = Board(9, 9)
     instance.select(1,1)
     instance.place_number(4)
-    print(instance.get_box(0,0))
-
+    print(instance.get_row(0))
 
 if __name__ == "__main__":
-    test_Board()
+    test_SudokuGenerator()
+    #test_Board()
     
