952df6ab300d0fbcedc07039b8723d1235bc023c
Sandro Mocevic <sandro.mocev@gmail.com>
2023-11-18 20:08:20 -0500

    file for testing sudoku methods

--- sudoku_tester.py
+++ sudoku_tester.py
@@ -1,6 +1,7 @@
 from sudoku_generator import SudokuGenerator
+from board import Board
 
-if __name__ == "__main__":
+def test_SudokuGenerator():
     instance = SudokuGenerator(9, 30)
     #Should Print 2D 9x9 list of 0's
     instance.print_board()
@@ -9,3 +10,15 @@ if __name__ == "__main__":
 
     #Should Print valid Sudoku Solution
     instance.print_board()
+
+
+def test_Board():
+    instance = Board(9, 9)
+    instance.select(1,1)
+    instance.place_number(4)
+    print(instance.get_box(0,0))
+
+
+if __name__ == "__main__":
+    test_Board()
+    
