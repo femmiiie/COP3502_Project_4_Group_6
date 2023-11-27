2a48f1cdbc389e4080a89d50e2847e403a69a02c
Sandro Mocevic <sandro.mocev@gmail.com>
2023-11-27 16:57:21 -0500

    changed variable names cus they made no sense

--- sudoku.py
+++ sudoku.py
@@ -46,16 +46,16 @@ def end_game()->None:
 def draw_button(screen:Surface, x_pos:int, y_pos:int, border_size:int, text:str, text_color:tuple, background_color:tuple)->Rect:
     text_object = DEFAULT_FONT.render(text, 0, text_color)
 
-    sizes = (text_object.get_width() + border_size, text_object.get_height() + border_size)
-    box_pos = (x_pos - (sizes[0]//2), y_pos - (sizes[1]//2))
+    button_size = (text_object.get_width() + border_size, text_object.get_height() + border_size)
+    text_position = (x_pos - (button_size[0]//2), y_pos - (button_size[1]//2))
 
-    text_center_pos = (box_pos[0] - (border_size//2), box_pos[1] - (border_size//2))
-    coords = pygame.Rect(text_center_pos[0], text_center_pos[1], sizes[0], sizes[1])
+    box_position = (text_position[0] - (border_size//2), text_position[1] - (border_size//2))
+    coordinates = pygame.Rect(box_position[0], box_position[1], button_size[0], button_size[1])
     
-    pygame.draw.rect(screen, background_color, coords)
-    screen.blit(text_object, box_pos)
+    pygame.draw.rect(screen, background_color, coordinates)
+    screen.blit(text_object, text_position)
 
-    return coords
+    return coordinates
         
 #True if area clicked, False otherwise
 def check_if_pressed(mouse_pos:tuple[int, int], button:Rect)->bool:
