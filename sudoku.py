4e0eca5be4d0442ba0e9f3c3cad51a10fc327d2f
Sandro Mocevic <sandro.mocev@gmail.com>
2023-11-28 19:50:26 -0500

    this code is bananas

--- sudoku.py
+++ sudoku.py
@@ -41,20 +41,24 @@ def render_game(screen: Surface, mouse_pos: tuple[int, int], current_event: int)
     screen.fill('red')
     box_size = 50
     offsets = (200,450)
+    
+    #don't ask how i figured this out i have no idea
+    #makes it so box lines are accented
+    width_matrix = [0, 1, 2, 2, 3, 4, 4, 5, 6, 7]
     cell_font = pygame.font.Font(None, box_size)
     
     # sketches board and cells
     for i in range(9):
         for j in range(9):
+            distances = ((i*box_size + offsets[1]) - width_matrix[i], (j*box_size + offsets[0]) - width_matrix[j])
             if globals.board.board[i][j].get_cell_value() != 0:
-                distances = (i*box_size + offsets[1], j*box_size + offsets[0])
-                pygame.draw.rect(screen, BLACK, (distances[0], distances[1], box_size + 1, box_size + 1), width=3, )
-
+                
+                box_coordinates = pygame.draw.rect(screen, BLACK, (distances[0], distances[1], box_size + 1, box_size + 1), 3)
                 cell_surf = cell_font.render(str(globals.board.board[i][j].get_cell_value()), 0, BLACK)
-                cell_rect = cell_surf.get_rect(center=(i * box_size + (offsets[1] + box_size/2), j * box_size + (offsets[0] + box_size/2)))
+                cell_rect = cell_surf.get_rect(center=box_coordinates.center)
                 screen.blit(cell_surf, cell_rect)
             else:
-                pygame.draw.rect(screen, BLACK, (i * box_size + offsets[1], j * box_size + offsets[0], box_size + 1, box_size + 1), width=3, )
+                pygame.draw.rect(screen, BLACK, (distances[0], distances[1], box_size + 1, box_size + 1), width=3, )
 
 
 if __name__ == "__main__":
